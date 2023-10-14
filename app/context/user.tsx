32009d0519ba0f0652755407c2be81acb89ddb99
Tasin <tasin0.net@gmail.com>
2023-10-14 15:17:53 -0400

    add: useCreateProfile hook

--- app/context/user.tsx
+++ app/context/user.tsx
@@ -9,11 +9,38 @@ import React, {
 } from "react";
 import { account, ID } from "@/libs/AppWriteClient";
 import { useRouter } from "next/navigation";
-import { UserContextTypes } from "../types";
+import { User, UserContextTypes } from "../types";
+import useGetProfileByUserId from "../hooks/useGetProfileByUserId";
 
 const UserContext = createContext<UserContextTypes | null>(null);
 
 const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
+
+  const checkUser = async () => {
+    try {
+      const currentSession = await account.getSession("current");
+      if (!currentSession) return;
+
+      const promise = (await account.get()) as any;
+      const profile = await useGetProfileByUserId(promise?.$id);
+
+      setUser({
+        id: promise?.$id,
+        name: promise?.name,
+        bio: profile?.bio,
+        image: profile?.image,
+      });
+    } catch (error) {
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
   return (
     <UserContext.Provider value={{ user, register, login, logout, checkUser }}>
       {children}
